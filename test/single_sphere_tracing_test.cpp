03debe7f2310e1e21bb59ea3b11c4524f7d6e5de
shivaduke28 <shivaduke28@gmail.com>
2023-07-19 00:04:21 +0900

    signle sphere tracing test

---
+++ test/single_sphere_tracing_test.cpp
@@ -0,0 +1,40 @@
+
+#include "ray.hpp"
+#include "image.hpp"
+#include "gtest/gtest.h"
+#include "camera.hpp"
+#include "vec3.hpp"
+#include "sphere.hpp"
+
+TEST(SingleSphereTracing, Image)
+{
+    Image img(512, 512);
+    PinholeCamera cam(Vec3(0, 0, 4), Vec3(0, 0, -1), 1);
+    Sphere sphere(Vec3(0, 0, 0), 1);
+    Vec3 light = normalize(Vec3(1, 1, 1));
+
+    double widthInv = 1.0 / img.width;
+    double heigthtInv = 1.0 / img.height;
+
+    for (int i = 0; i < img.width; i++)
+    {
+        double u = 2.0 * i * widthInv - 1.0;
+        for (int j = 0; j < img.height; j++)
+        {
+            double v = 2.0 * j * heigthtInv - 1.0;
+            Ray ray = cam.getRay(-u, -v);
+            Hit res;
+            if (sphere.intersect(ray, res))
+            {
+                Vec3 n = res.hitNormal;
+                double lambert = std::max(0.0, dot(res.hitNormal, light));
+                img.setPixel(i, j, Vec3(1, 1, 1) * lambert);
+            }
+            else
+            {
+                img.setPixel(i, j, Vec3(0, 0, 0));
+            }
+        }
+    }
+    img.ppmOutput("single_sphere_tracing_test.ppm");
+}
